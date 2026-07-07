f08b9e34b1b2fdf4376fd033f664c508e3367b33
Inverated <120025059+Inverated@users.noreply.github.com>
2026-07-08 02:51:04 +0800

    test

--- proa_advisor/index.js
+++ proa_advisor/index.js
@@ -41,10 +41,12 @@ app.get("/data_stream", (req, res) => {
     res.flushHeaders?.();
 
     add_client(res);
+    console.log(`Client connected. Total clients: ${get_clients().size}`);
     res.write(": connected\n\n");
 
     req.on("close", () => {
         remove_client(res);
+        console.log(`Client disconnected. Total clients: ${get_clients().size}`);
     });
 })
 
